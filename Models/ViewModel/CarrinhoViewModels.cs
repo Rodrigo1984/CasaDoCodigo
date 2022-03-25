8cc640fa9951939a6e5797433575e71fdde1d3c0
dev <dev@LAPTOP-LS4A6J63>
2022-03-25 12:33:43 -0300

    Add project files.

---
+++ Models/ViewModel/CarrinhoViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class CarrinhoViewModel
+    {
+        public List<ItemPedido> Itens { get; private set; }
+        public decimal Total {
+            get
+            {
+                return Itens.Sum(i => i.Subtotal);
+            }
+        }
+        public CarrinhoViewModel(List<ItemPedido> itens)
+        {
+            this.Itens = itens;
+        }
+    }
+}
---
+++ contexto.cs
@@ -0,0 +1,11 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+    public class contexto : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ItemPedido> itensPedidos  { get; set; }
+    }
+}
